c0ca51859fcdcea8cb87ab3b398c58cfa64fa5b6
Aravind Bandipelli <aravindbandipelli@gmail.com>
2025-02-09 15:41:26 -0500

    Refined Resume & Certifications Layout

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -6,10 +6,28 @@ import { FiDownload } from "react-icons/fi";
 const timelineItems = [
   {
     type: "Education",
-    title: "Master of Science in Management – Data Analytics",
+    title: "Master of Science in Data Analytics",
     institution: "Indiana Wesleyan University",
     year: "Present",
     gpa: "GPA: 3.825/4.0",
+  },
+  // {
+  //   type: "Certification",
+  //   title: "Google Data Analytics Professional Certificate",
+  //   institution: "Google (In Progress)",
+  //   year: "2024",
+  // },
+  {
+    type: "Certification",
+    title: "Microsoft Certified: Azure Data Scientist Associate",
+    institution: "Microsoft",
+    year: "2023",
+  },
+  {
+    type: "Certification",
+    title: "IBM Data Science Professional Certificate",
+    institution: "IBM",
+    year: "Completed",
   },
   {
     type: "Certification",
@@ -19,17 +37,24 @@ const timelineItems = [
   },
   {
     type: "Certification",
-    title: "Azure Developer Associate",
-    institution: "Microsoft",
-    year: "2023",
+    title: "Infosys Certified Angular Professional",
+    institution: "Infosys",
+    year: "Completed",
   },
   {
     type: "Certification",
-    title: "Google Data Analytics Professional",
-    institution: "Google (In Progress)",
-    year: "2024",
+    title: "Infosys Certified C# Programmer",
+    institution: "Infosys",
+    year: "Completed",
+  },
+  {
+    type: "Certification",
+    title: "Infosys Certified .NET Microservices Developer",
+    institution: "Infosys",
+    year: "Completed",
   }
 ];
+
 
 const Resume = () => {
   return (
@@ -64,7 +89,7 @@ const Resume = () => {
         {/* Download Resume Button */}
         <div className="text-center mt-8">
           <a 
-            href="/assets/Aravind_Bandipelli_Resume.pdf"
+            href="/Aravind_Bandipelli_Resume.pdf"
             download="Aravind_Bandipelli_Resume.pdf"
             className="inline-flex items-center gap-2 px-4 py-2 bg-blue-600 hover:bg-blue-500 text-white text-sm font-medium rounded-md shadow-md transition duration-300"
           >
